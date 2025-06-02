2d29f95e9140d988bc1875404b72887c766099f4
nguyenktrung <khanhtrung13@gmail.com>
2025-06-02 13:52:54 +0700

    up

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -3,6 +3,15 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Book } from 'lucide-vue-next';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -16,7 +25,7 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Dashboard" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+        <!-- <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
@@ -30,6 +39,23 @@ const breadcrumbs: BreadcrumbItem[] = [
             </div>
             <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
                 <PlaceholderPattern />
+            </div>
+        </div> -->
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <Card class="gap-2">
+                    <CardHeader class="flex flex-row items-center justify-between space-y-0">
+                        <CardTitle class="text-sm font-medium">
+                            Total Book
+                        </CardTitle>
+                        <Book class="h-4 w-4"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div class="text-2xl font-bold">
+                            100
+                        </div>
+                    </CardContent>
+                </Card>
             </div>
         </div>
     </AppLayout>
