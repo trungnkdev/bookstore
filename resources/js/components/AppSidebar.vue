b1c836c23ab3d5083c1f9ff987e5abac7f0e4678
nguyenktrung <khanhtrung13@gmail.com>
2025-06-09 22:12:24 +0700

    up

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid, Tag, LibraryBig, User, ShoppingBasket, Store } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, Tag, LibraryBig, User, ShoppingBasket, Store, BookCopy } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -15,14 +15,24 @@ const mainNavItems: NavItem[] = [
         icon: LayoutGrid,
     },
     {
+        title: 'Products',
+        href: '/admin/products',
+        icon: LibraryBig,
+    },
+    {
         title: 'Categories',
         href: '/categories',
         icon: Tag,
     },
     {
-        title: 'Products',
-        href: '/admin/products',
-        icon: LibraryBig,
+        title: 'Tags',
+        href: '/tags',
+        icon: Tag,
+    },
+    {
+        title: 'Collection',
+        href: '/collections',
+        icon: BookCopy,
     },
     {
         title: 'Orders',
